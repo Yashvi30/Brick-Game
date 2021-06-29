5b8cb5f733b5079c8b35eac631e74a0e1c4bbeb2
Yashvi <yashvigarg330@gmail.com>
2021-06-30 02:30:55 +0530

    done

---
+++ score card/script.js
@@ -0,0 +1,8 @@
+let playAgain = document.querySelector(".playAgain");
+playAgain.addEventListener("click", () => {
+  location.href = "https://shauryasrivatava.github.io/Game/";
+});
+let nextLevel = document.querySelector(".nextLevel");
+nextLevel.addEventListener("click", () => {
+  location.href = "https://shauryasrivatava.github.io/Game/";
+});
